a73b37988e4cf96d5c43650c6a23e4c54c458f42
PTGABUT <akunbabu69@gmail.com>
2022-02-22 08:06:38 +0700

    upload modul 5

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -25,8 +25,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{route ('login')}}">Login</a>
                 </li>
-            </u1> 
-            <u1 class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{route ('register')}}">Register</a>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,6 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function (){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-});
 
+});
 
